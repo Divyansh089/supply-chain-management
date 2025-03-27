aa7aba17b56af966c9e300e530f673651bf90e86
Divyansh Patel <pateldivyansh131@gmail.com>
2025-03-28 02:27:47 +0530

    Adding Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import "../styles/globals.css"
 
 //Internal Import
-import {TrackingProvider} from "../Context/Tracking";
+import {TrackingProvider} from "../Context/TrackingContext";
+
+//import {NavBar , Footer} from "../Components";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer"; 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <TrackingProvider>
+      <NavBar/>
       <Component {...pageProps} />
     </TrackingProvider>
+    <Footer/>
     </>
 
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,95 @@
-import React from "react";
+import React , { useState , useEffect , useContext} from "react";
+
+//Internal Import
+import {
+  Table,
+  Form,
+  Services,
+  Profile,
+  CompleteShipment,
+  GetShipment,
+  StartShipment,
+
+} from "../Components/index";
+// console.log("Table:", Table);
+// console.log("Form:", Form);
+// console.log("Services:", Services);
+// console.log("Profile:", Profile);
+// console.log("CompleteShipment:", CompleteShipment);
+// console.log("GetShipment:", GetShipment);
+// console.log("StartShipment:", StartShipment);
+
+
+import {TrackingContext} from  "../Context/TrackingContext";
 
 const index = () => {
-  return <div>index</div>;
+  const {
+    currentUser, 
+    createShipment,
+    getAllShipment,
+    completeShipment,
+    getShipment,
+    startShipment,
+    getShipmentCount,
+  } = useContext(TrackingContext);
+
+  //State Variable
+  const [createShipmentModal, setCreateShipmentModal] = useState(false);
+  const [openProfile , setOpenProfile] = useState(false);
+  const [startModal , setStartModal] = useState(false);
+  const [completeModal , setCompleteModal] = useState(false);
+  const [getModal , setGetModal] = useState(false);
+
+  // Data State Variable
+  const [allShipmentsData, setallShipmentsData] = useState();
+  useEffect(() => {
+    const getCampaignsData = getAllShipment();
+    return async () => {
+      const allData = await getCampaignsData;
+      setallShipmentsData(allData);
+    };
+  }, []);
+
+    return (
+      <>
+        <Services 
+        setOpenProfile = {setOpenProfile}
+        setCreateShipmentModal = {setCreateShipmentModal}
+        setGetModal = {setGetModal}
+        setStartModal = {setStartModal}
+        />
+        <Table
+          setCreateShipmentModal = {setCreateShipmentModal}
+          allShipmentsData = {allShipmentsData}
+        />
+        <Form
+          createShipmentModal = {createShipmentModal}
+          createShipment = {createShipment}
+          setCreateShipmentModal = {setCreateShipmentModal}
+        />
+        <Profile
+          openProfile = {openProfile}
+          setOpenProfile = {setOpenProfile}
+          currentUser = {currentUser}
+          getShipmentCount = {getShipmentCount}
+        />
+        <CompleteShipment
+          completeModal = {completeModal}
+          setCompleteModal = {setCompleteModal}
+          completeShipment = {completeShipment}
+        />
+        <GetShipment
+          getModal = {getModal}
+          setGetModal = {setGetModal}
+          getShipment = {getShipment}
+        />
+        <StartShipment
+          startModal = {startModal}
+          setStartModal = {setStartModal}
+          startShipment = {startShipment}
+        />
+      </>
+    );
 };
 
 export default index;
